5cc4714f9833ae31247ea23dfffcff0dbee34b18
Leandro Serra <leandro.serra.10@gmail.com>
2020-11-04 20:33:03 -0300

    1º Deploy no Heroku

--- djangoecommerce/core/views.py
+++ djangoecommerce/core/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
+# coding=utf-8
 
+from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+
 def index(request):
-    texts = ['Lorem ipsum sit amet', 'consectur adipiscing elit']
-    context = {
-        'title' : 'Django E-commerce',
-        'teste' : 'Teste Context',
-        'texts' : texts
-    }
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def product_list(request):
+    return render(request, 'product_list.html')
+
+
+def product(request):
+    return render(request, 'product.html')
